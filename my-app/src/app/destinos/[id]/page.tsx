cd16efe90e958dfc2f53bf004a48bcdad51ba8d8
Igor Augusto <igoraf.dev33@gmail.com>
2025-11-06 16:04:19 -0300

    Correção das páginas dinâmicas e adição de estilização para as mesmas

--- my-app/src/app/destinos/[id]/page.tsx
+++ my-app/src/app/destinos/[id]/page.tsx
@@ -1,26 +1,28 @@
 import { destinos } from "@/app/libs/destinos"
-import { Trip } from "@/app/types/Types"
+import styles from "./page.module.css"
 
 type Params = {
-    params: {
+    params: Promise<{
         id: string
-    }
+    }>
 }
 
 async function DestinoDetails({ params }: Params) {
-    const destino: Trip = destinos.find((dados) => dados.id === params.id)!
+    const {id} = await params
 
-    console.log(destino)
+    const destino = destinos.find(dados => dados.id === id);
 
     if (!destino) return <h1>Destino não encontrado</h1>
 
     return (
-        <main>
-            <img src={destino.imagem} alt={`Imagem do seu destino em ${destino.nome}`} />
-            <div>
-                <h2>{destino.nome}</h2>
-                <p>{destino.descricao}</p>
-                <button>Agende a sua viagem</button>
+        <main className={styles.main}>
+            <div className={styles.main__card}>
+                <img src={destino.imagem} alt={`Imagem do seu destino em ${destino.nome}`} className={styles.card__image} />
+                <div className={styles.card__textContainer}>
+                    <h2 className={styles.card__title}>{destino.nome}</h2>
+                    <p className={styles.card__text}>{destino.descricao}</p>
+                    <button className={styles.card__button}>Agende a sua viagem</button>
+                </div>
             </div>
         </main>
     )
